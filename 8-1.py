0435add5c8f889e6f39dc5aaf9da1a71dd3ca4d4
Buck Porter <dporter360@gmail.com>
2017-10-28 17:26:40 -0600

    User chooses parity in cmd line

--- 8-1.py
+++ 8-1.py
@@ -1,15 +1,32 @@
 #!/usr/bin python
+import sys
+
 count = 0
 
-usrInput = raw_input("8 bit input: ")
-byte = map(int,str(usrInput))
+def evenOdd(usrInput, parity):
+	if parity == 'e':
+		byte = map(int,str(usrInput))
+		count = 0
+		for i in byte:
+			if i == 1:
+				count += 1
+		if count % 2 == 0:
+			byte += [1]
+			print ''.join(map(str, byte))
+		else:
+			print("Odd parity")
+	
+	elif parity == 'o':
+		byte = map(int,str(usrInput))
+		count = 0
+		for i in byte:
+			if i == 1:
+				count += 1
+		if count % 2 != 0:
+			byte += [0]
+			print ''.join(map(str,byte))
+		else:
+			print("Even parity")
+	sys.exit()
 
-for i in byte:
-	if i == 1:
-		count += 1
-
-if count % 2 == 0:
-	byte += [1]
-	print ''.join(map(str, byte))
-else:
-	print("Odd parity")
+evenOdd(sys.argv[1], sys.argv[2])
